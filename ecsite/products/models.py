95c6abb8af92ad94becbf53ed4a0116473f08670
stanaka <stanaka1306@gmail.com>
2026-06-11 13:11:05 +0900

    fix applications

--- ecsite/products/models.py
+++ ecsite/products/models.py
@@ -2,20 +2,6 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 
-class User(models.Model):
-
-    class Meta:
-        db_table = "account_user"
-
-    user_id = models.CharField(verbose_name="会員ID", max_length=128, primary_key=True, db_index=True)
-    password = models.CharField(verbose_name="パスワード", max_length=256)
-    name = models.CharField(verbose_name="名前", max_length=128)
-    address = models.CharField(verbose_name="住所", max_length=256)
-
-    def __str__(self):
-        return self.name
-    
-
 class Category(models.Model):
 
     class Meta:
@@ -40,7 +26,7 @@ class Item(models.Model):
     price = models.IntegerField(verbose_name="価格")
     stock = models.IntegerField(verbose_name="在庫数")
     recommended = models.BooleanField(verbose_name="オススメ", max_length=1, default=False)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # article = models.ForeignKey(Article, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -55,11 +41,11 @@ class ShoppingCart(models.Model):
     # id = models.IntegerField(verbose_name="ID", primary_key=True, autoincrement=True, db_index=True)
     amount = models.IntegerField(verbose_name="数量")
     booked_date = models.DateTimeField(verbose_name="登録日", auto_now_add=True)
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user_id
+        return self.user
 
 
 class Purchase(models.Model):
@@ -71,7 +57,7 @@ class Purchase(models.Model):
     destination = models.CharField(verbose_name="配送先", max_length=256)
     booked_date = models.DateTimeField(verbose_name="注文日", auto_now_add=True)
     cancel = models.BooleanField(verbose_name="キャンセル", max_length=1, default=False)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.purchase_id
@@ -83,23 +69,12 @@ class PurchaseDetail(models.Model):
 
     purchase_detail_id = models.IntegerField(verbose_name="注文詳細ID", primary_key=True, db_index=True)
     amount = models.IntegerField(verbose_name="注文数", validators=[MinValueValidator(1)])
-    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
-    purchase_id = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.purchase_detail_id
-    
 
-class Admin(models.Model):
-
-    class Meta:
-        db_table = "administrator_admin"
-
-    admin_id = models.CharField(verbose_name="管理者ID", max_length=128, primary_key=True, db_index=True)
-    password = models.CharField(verbose_name="パスワード", max_length=256)
-
-    def __str__(self):
-        return self.admin_id
 
 
 
