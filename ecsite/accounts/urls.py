eb777e37722ea07005921d97fc312c881ad04a1f
stanaka <stanaka1306@gmail.com>
2026-06-12 08:50:32 +0900

    iroiro yarimashita

--- ecsite/accounts/urls.py
+++ ecsite/accounts/urls.py
@@ -3,13 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    # path('search/', views.Top.as_view(), name='top'),
-    # path('showResult/', views.SearchResult.as_view(), name='search_result'),
-    # path('itemDetail/', views.ItemDetail.as_view(), name='item_detail'),
-    # path('cart/', views.Cart.as_view(), name='cart'),
-    # path('login/', views.Login.as_view(), name='login'),
-    # path("registerUser/", views.RegisterUser.as_view(), name="register_user"),
-    # path("registerUserConfirm/", views.RegisterUserConfirm.as_view(), name="register_user_confirm"),
+    path('login/', views.Login.as_view(), name='login'),
+    path("registerUser/", views.RegisterUser.as_view(), name="register_user"),
+    path("registerUserConfirm/", views.RegisterUserConfirm.as_view(), name="register_user_confirm"),
+    path("registerUserCommit/", views.RegisterUserCommit.as_view(), name="register_user_commit"),
     # path("userInfo/", views.UserInfo.as_view(), name="user_info"),
     # path("updateUser/", views.UpdateUser.as_view(), name="update_user"),
     # path("updateUserConfirm/", views.updateUserConfirm.as_view(), name="update_user_confirm"),
