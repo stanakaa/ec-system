8e7cf5d0c31a29865593345109d88c36087b6557
stanaka <stanaka1306@gmail.com>
2026-06-12 15:40:12 +0900

    create itemDetail

--- ecsite/products/forms.py
+++ ecsite/products/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from .models import Category
+
 
 
 class SearchForm(forms.Form):
@@ -6,20 +8,35 @@ class SearchForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.label_suffix = ""
 
-    category = forms.ChoiceField(
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all().order_by("category_id"),
         label="カテゴリ",
-        choices=(
-            ("all", "すべて"),
-            ("hat", "帽子"),
-            ("bag", "鞄"),
-        ),
-        required=True,
-        widget=forms.Select(attrs={"class": "form-control"})
+        required=False,
+        empty_label="すべて"
     )
 
-    keyword = forms.CharField(
-        label="キーワード",
-        max_length=255,
-        required=False,
-        widget=forms.TextInput(attrs={"class": "form-control"})
-    )
+    keyword = forms.CharField(label="キーワード", required=False, max_length=100)
+
+
+# class SearchForm(forms.Form):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.label_suffix = ""
+
+#     category = forms.ChoiceField(
+#         label="カテゴリ",
+#         choices=(
+#             ("all", "すべて"),
+#             ("hat", "帽子"),
+#             ("bag", "鞄"),
+#         ),
+#         required=True,
+#         widget=forms.Select(attrs={"class": "form-control"})
+#     )
+
+#     keyword = forms.CharField(
+#         label="キーワード",
+#         max_length=255,
+#         required=False,
+#         widget=forms.TextInput(attrs={"class": "form-control"})
+#     )
--- ecsite/products/views.py
+++ ecsite/products/views.py
@@ -15,33 +15,29 @@ class Top(View):
             "login_name": request.session.get("name"),
         }
         return render(request, "main.html", context)
-    
+
 
 class ShowResult(View):
     def get(self, request, *args, **kwargs):
-        form = SearchForm(request.GET)
+        form = SearchForm(request.GET or None)
+        items = Item.objects.select_related("category").all()
 
-        items = Item.objects.all()
+        keyword = ""
+        category_name = "すべて"
 
         if form.is_valid():
             category = form.cleaned_data["category"]
-            keyword = form.cleaned_data["keyword"]
+            keyword = form.cleaned_data["keyword"] or ""
 
-            category_dict = dict(form.fields["category"].choices)
-            category_name = category_dict[category]
-
-            if category == "hat":
-                items = items.filter(category__name="帽子")
-
-            elif category == "bag":
-                items = items.filter(category__name="鞄")
-
-            # all のときはカテゴリで絞り込まない
+            if category:
+                items = items.filter(category=category)
+                category_name = category.name
 
             if keyword:
-                items = items.filter(name__contains=keyword)
+                items = items.filter(name__icontains=keyword)
 
         context = {
+            "form": form,
             "items": items,
             "keyword": keyword,
             "category_name": category_name,
@@ -49,6 +45,34 @@ class ShowResult(View):
             "login_name": request.session.get("name"),
         }
         return render(request, "searchResult.html", context)
+
+# class ShowResult(View):
+#     def get(self, request, *args, **kwargs):
+#         form = SearchForm(request.GET)
+
+#         items = Item.objects.all()
+
+#         if form.is_valid():
+#             category = form.cleaned_data["category"]
+#             keyword = form.cleaned_data["keyword"]
+
+#             category_dict = dict(form.fields["category"].choices)
+#             category_name = category_dict[category]
+
+#             if category != "all":
+#                 items = items.filter(category=category)
+
+#             if keyword:
+#                 items = items.filter(name__contains=keyword)
+
+#         context = {
+#             "items": items,
+#             "keyword": keyword,
+#             "category_name": category_name,
+#             "login_user_id": request.session.get("user_id"),
+#             "login_name": request.session.get("name"),
+#         }
+#         return render(request, "searchResult.html", context)
     
 
 class ItemDetail(View):
