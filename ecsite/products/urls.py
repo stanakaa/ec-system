5df40e2d7a8ebe28089f29fcde9143c4a907d975
stanaka <stanaka1306@gmail.com>
2026-06-15 09:21:12 +0900

    bag fix

--- ecsite/products/urls.py
+++ ecsite/products/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('search/', views.Top.as_view(), name='top'),
     path('showResult/', views.ShowResult.as_view(), name='show_result'),
     path('itemDetail/<int:item_id>', views.ItemDetail.as_view(), name='item_detail'),
-    # path('cart/', views.Cart.as_view(), name='cart'),
+    path('addCart/<int:item_id>', views.AddCart.as_view(), name='add_cart'),
+    path('cart/', views.ShowCart.as_view(), name="cart"),
 ]
